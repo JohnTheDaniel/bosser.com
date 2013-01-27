1af9588ddb08886ed5a50864bb00f9dff7881220
John Daniel <daniel@bosser.com>
2013-01-27 02:06:18 +0100

    Gjorde navigationen uppdateringsbar

---
+++ navwrap.php
@@ -0,0 +1,64 @@
+    <div id="navwrap">
+        <div id="navigation">
+            <!--barnen start, länk till daniel, jenny och simon-->
+
+            <div id="barnen" style="display:none;">
+                <div id="daniel" class="barnen">
+                    <a href="http://www.thebananaworld.com/johndaniel/index.html"></a>
+
+                    <div>
+                        <center>
+                            Daniel
+                        </center>
+                    </div>
+                </div>
+
+                <div id="jenny" class="barnen">
+                    <a href="jenny/index.html"></a>
+
+                    <div>
+                        <center>
+                            Jenny
+                        </center>
+                    </div>
+                </div>
+
+                <div id="simon" class="barnen">
+                    <a href="simon/index.html"></a>
+
+                    <div>
+                        <center>
+                            Simon
+                        </center>
+                    </div>
+                </div>
+            </div><!--länkar start-->
+
+            <div id="hem-index" onclick="window.location.href='index.html'">
+                <div class="meny">
+                    <center>
+                        <h3><i>Hem</i></h3>
+                    </center>
+                </div>
+            </div>
+
+            <div id="kontakt" onclick="window.location.href='kontaktaoss.html'">
+                <div class="meny">
+                    <center>
+                        <h2>Kontakta</h2>
+                    </center>
+                </div>
+            </div>
+
+            <div id="baten" onclick="window.location.href='baten.html'">
+                <div class="meny">
+                    <center>
+                        <h2>Båten</h2>
+                    </center>
+                </div>
+            </div>
+        </div>
+
+        <div id="header-image"></div>
+    </div>
+
